4e891e48ffd37c16c4076a57d72acdc94d80d65d
Tomasz Grande <grande.tomasz@gmail.com>
2018-05-14 21:15:45 +0200

    Format code

--- random-quote.js
+++ random-quote.js
@@ -1,42 +1,45 @@
 // VARIABLES
-var quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
-var tweetLink = 'https://twitter.com/intent/tweet?text=';
-var prefix = 'https://cors-anywhere.herokuapp.com/';
+var quoteUrl =
+  "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 // FUNCTIONS
 function getQuote() {
   $.getJSON(prefix + quoteUrl, createTweet);
-  $.ajaxSetup({ 
-    cache: false 
+  $.ajaxSetup({
+    cache: false
   });
 }
 
 function createTweet(input) {
   var data = input[0];
 
-  var quoteText = $(data.content).text().trim();
+  var quoteText = $(data.content)
+    .text()
+    .trim();
   var quoteAuthor = data.title;
 
   if (!quoteAuthor.length) {
-    quoteAuthor = 'Unknown author';
+    quoteAuthor = "Unknown author";
   }
 
-  var tweetText = 'Quote: ' + quoteText + '\nAuthor: ' + quoteAuthor;
+  var tweetText = "Quote: " + quoteText + "\n" + "Author: " + quoteAuthor;
 
   if (tweetText.length > 140) {
     getQuote();
   } else {
     var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text('Author: ' + quoteAuthor);
-    $('.tweet').attr('href', tweet);
+    $(".quote").text(quoteText);
+    $(".author").text("Author: " + quoteAuthor);
+    $(".tweet").attr("href", tweet);
   }
 }
 
 // MAIN
 $(document).ready(function() {
   getQuote();
-  $('.trigger').click(function() {
+  $(".trigger").click(function() {
     getQuote();
   });
 });
